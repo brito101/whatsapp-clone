a7093d4765ab378a2cc5d06ec0f4010a67f95b4b
brito101 <rodrigobrito101@hotmail.com>
2024-03-21 18:14:03 -0300

    add: getList

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import DonutLargeIcon from "@mui/icons-material/DonutLarge";
-import ChatIcon from "@mui/icons-material/Chat";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import SearchIcon from "@mui/icons-material/Search";
-import { useState } from "react";
 
-import ChatListItem from "./components/ChatListIem";
+import Api from "./Api";
+
+import ChatListItem from "./components/ChatListItem";
 import ChatIntro from "./components/ChatIntro";
 import ChatWindow from "./components/ChatWindow";
 import NewChat from "./components/NewChat";
 import Login from "./components/Login";
 
+import { DonutLarge, Chat, MoreVert, Search } from "@mui/icons-material";
+
 function App() {
-  const [chatList, setChatList] = useState([
-    {
-      chatId: 1,
-      title: "Bob",
-      image:
-        "https://www.rodrigobrito.dev.br/themes/web/assets/images/avatar.webp",
-    },
-    {
-      chatId: 2,
-      title: "Alice",
-      image:
-        "https://www.rodrigobrito.dev.br/themes/web/assets/images/avatar.webp",
-    },
-  ]);
+  const [chatlist, setChatList] = useState([]);
   const [activeChat, setActiveChat] = useState({});
   const [user, setUser] = useState(null);
   const [showNewChat, setShowNewChat] = useState(false);
+
+  useEffect(() => {
+    if (user !== null) {
+      let unsub = Api.onChatList(user.id, setChatList);
+      return unsub;
+    }
+  }, [user]);
 
   const handleNewChat = () => {
     setShowNewChat(true);
@@ -40,6 +34,7 @@ function App() {
       name: u.displayName,
       avatar: u.photoURL,
     };
+    await Api.addUser(newUser);
     setUser(newUser);
   };
 
@@ -51,51 +46,51 @@ function App() {
     <div className="app-window">
       <div className="sidebar">
         <NewChat
+          chatlist={chatlist}
           user={user}
-          chatList={chatList}
           show={showNewChat}
           setShow={setShowNewChat}
         />
         <header>
-          <img
-            className="header-avatar"
-            src={user.avatar ? user.avatar : "./images/avatar.png"}
-            alt=""
-          />
-          <div className="header-buttons">
-            <div className="header-btn">
-              <DonutLargeIcon style={{ color: "#919191" }} />
+          <img className="header--avatar" src={user.avatar} alt="" />
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLarge style={{ color: "#919191" }} />
             </div>
-            <div className="header-btn" onClick={handleNewChat}>
-              <ChatIcon style={{ color: "#919191" }} />
+            <div onClick={handleNewChat} className="header--btn">
+              <Chat style={{ color: "#919191" }} />
             </div>
-            <div className="header-btn">
-              <MoreVertIcon style={{ color: "#919191" }} />
+            <div className="header--btn">
+              <MoreVert style={{ color: "#919191" }} />
             </div>
           </div>
         </header>
+
         <div className="search">
-          <div className="search-input">
-            <SearchIcon fontSize="small" style={{ color: "#919191" }} />
+          <div className="search--input">
+            <Search fontSize="small" style={{ color: "#919191" }} />
             <input
               type="search"
               placeholder="Procurar ou começar uma nova conversa"
             />
           </div>
         </div>
-        <div className="chatList">
-          {chatList.map((item, key) => (
+
+        <div className="chatlist">
+          {chatlist.map((item, key) => (
             <ChatListItem
-              active={activeChat.chatId === chatList[key].chatId}
               key={key}
               data={item}
-              onClick={() => setActiveChat(chatList[key])}
+              active={activeChat.chatId === chatlist[key].chatId}
+              onClick={() => setActiveChat(chatlist[key])}
             />
           ))}
         </div>
       </div>
-      <div className="content-area">
-        {activeChat.chatId !== undefined && <ChatWindow user={user} />}
+      <div className="contentarea">
+        {activeChat.chatId !== undefined && (
+          <ChatWindow user={user} data={activeChat} />
+        )}
         {activeChat.chatId === undefined && <ChatIntro />}
       </div>
     </div>
